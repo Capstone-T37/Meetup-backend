54c1c0fe49033c02c2b54e64bcfeb1acf75c79aa
ayman fakri <61848289+afakri@users.noreply.github.com>
2023-01-25 18:25:01 -0500

    made authentication fully server side

--- authentication-microservice/api/routes.js
+++ authentication-microservice/api/routes.js
@@ -5,6 +5,8 @@ const controller = require('./controller');
 module.exports = (app) => {
     app.route('/')
         .get(controller.root);
+    app.route('/api/v1/protected')
+        .post(controller.protected)
     app.route('/api/v1/login')
         .post(controller.login)
     app.route('/api/v1/signup')
