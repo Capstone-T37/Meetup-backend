8c72db49cf632634b3c96115589d43b9c0f455e6
ayman fakri <61848289+afakri@users.noreply.github.com>
2023-02-06 10:39:29 -0500

    using github actions now

--- scripts/infrastructure/main.tf
+++ scripts/infrastructure/main.tf
@@ -1,35 +1,39 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.0"
-    }
-  }
   backend "azurerm" {
-    resource_group_name = var.bkstrgrg
-    storage_account_name = var.bkstrg
-    container_name = var.bkcontainer
-    key = var.bkstrgkey
+    resource_group_name  = "tamopstfstates"
+    storage_account_name = "tfstatedevops"
+    container_name       = "terraformgithubexample"
+    key                  = "terraformgithubexample.tfstate"
   }
 }
-
+ 
 provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
   features {}
 }
-
-resource "azurerm_resource_group" "meetup_rg" {
-  name     = var.resourcegroup_name
-  location = var.location
-  tags     = var.tags
+ 
+data "azurerm_client_config" "current" {}
+ 
+#Create Resource Group
+resource "azurerm_resource_group" "tamops" {
+  name     = "tamops"
+  location = "eastus2"
 }
-
-resource "azurerm_container_registry" "meetup_acr" {
-  name                = var.acr_name
-  resource_group_name = azurerm_resource_group.aks-rg.name
-  location            = var.location
-  sku                 = "Standard"
-  admin_enabled       = false
+ 
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "tamops-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = "eastus2"
+  resource_group_name = azurerm_resource_group.tamops.name
 }
-
-
-
+ 
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.tamops.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
+}
