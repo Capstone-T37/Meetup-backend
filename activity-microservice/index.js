a1fd577707392e7ef6dcbf18a6bba4aae2219b31
RiyadElazhari <59661625+riyad123456@users.noreply.github.com>
2023-02-28 19:29:49 -0500

    PR done

--- activity-microservice/index.js
+++ activity-microservice/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express();
 const routes = require('./api/routes');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3050;
 
 app.use(bodyParser.json())
 routes(app);
